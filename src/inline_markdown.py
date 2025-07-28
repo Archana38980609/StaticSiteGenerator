6fb96ea128ed1a5096cb0ef7d82cedfa38837338
Archana38980609 <archi.dtg@gmail.com>
2025-07-28 18:12:51 +0000

    feat: Implement Markdown link and image extraction
    introduce functionality to parse Markdown text and extract all link and image data using regular expressions.
    
    Two new functions have been created:
    
    - extract_markdown_links: Finds all occurrences of Markdown links (e.g., [anchor text](URL)) and returns a list of tuples, where each tuple contains the anchor text and the link URL.
    
    Comprehensive unit tests have been added to verify the correctness of both functions and ensure they handle various edge cases properly.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,5 @@
+import re
 from textnode import TextNode, TextType
-
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -22,4 +22,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
 
